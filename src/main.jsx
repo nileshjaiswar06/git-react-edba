601906c2c6599127d2a69edf667874f8e42fe401
Nilesh <nilesh.jaiswar006@gmail.com>
2025-03-29 12:19:20 +0530

    dummy component cretaed

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Dummy from './Components/dummy.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: '/dummy',
+    element: <Dummy /> // If you want to display "Dummy" content for this route
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
+    {/* <App /> */}
   </StrictMode>,
 )
